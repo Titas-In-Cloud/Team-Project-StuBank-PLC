e2aa67293ac9eb655882a781b996253638ef6ba1
jackb <j.c.bryan1@ncl.ac.uk>
2021-01-09 00:31:28 +0000

    Implemented AES

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,31 +7,6 @@ const auth = require("../middleware/auth");
 router.post("/register", async(req, res) => {
     try {
         let {email, password, passwordCheck, personalID, phoneNum, firstName, lastName} = req.body;
-        // validate
-        if(!email || !password || !passwordCheck || !personalID || !phoneNum || !firstName || !lastName)
-            return res.status(400).json({msg: "One or more required fields are blank"});
-        if (!(firstName.match(/^[A-Za-z\-]+$/) )||(!(lastName.match(/^[A-Za-z\-]+$/))))
-            return res.status(400).json({msg: "Please ensure only letters are used for the first name and last name fields"});
-        if(password !== passwordCheck)
-            return res.status(400).json({msg: "Enter password twice to ensure password has been entered correctly"});
-        const existingEmail = await User.findOne({email: email});
-        if(existingEmail)
-            return res.status(400).json({msg: "An account with this email already exists."});
-        const existingPID = await User.findOne({personalID: personalID});
-        if(existingPID)
-            return res.status(400).json({msg: "An account with this personal ID already exists."});
-        const existingPhoneNum = await User.findOne({phoneNum: phoneNum});
-        if(existingPhoneNum)
-            return res.status(400).json({msg: "An account with this phone number already exists."});
-        // if (!(phone.match(/\d{2}-\d{4}-\d{7}$/))) {
-        //     res.status(400).json({msg: "Telephone is not valid, please enter a valid phone number of the form XX--XXXX-XXXXXXX"});
-        // }
-        if (!(email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)))
-            return res.status(400).json({msg: "Email address is not valid, please enter a valid email, e.g. example@email.com"});
-        if (!(password.match(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,15}$/)))
-            return res.status(400).json({msg: "Password is not valid"});
-        if (!(personalID.match(/\d{11}$/)))
-            return res.status(400).json({msg: "Please enter exactly 11 digits for the personal ID number"});
 
         //encrypts data using aes, returns the encrypted data, the iv, and the key all as hexadecimal strings
         function aesEncrypt(data) {
@@ -43,42 +18,78 @@ router.post("/register", async(req, res) => {
             encrypted = Buffer.concat([encrypted, cipher.final()]);
             return {encryptedData: encrypted.toString('hex'), iv: iv.toString('hex'), key};
         }
-        /*example of how to use aesEncrypt
-        let example = aesEncrypt('example');
-        let exampleData = example.encryptedData;
-        console.log(example.iv);*/
+        function aesDecrypt(data) {
+            const crypto = require('crypto');
+            const key = String(data.get('key'));
+            const iv = Buffer.from(String(data.get('iv')), 'hex');
+            let encryptedText = Buffer.from(String(data.get('encryptedData')), 'hex');
+            let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+            let decrypted = decipher.update(encryptedText);
+            decrypted = Buffer.concat([decrypted, decipher.final()]);
+            return decrypted.toString();
+        }
+
+        // validate
+        if(!email || !password || !passwordCheck || !personalID || !phoneNum || !firstName || !lastName)
+            return res.status(400).json({msg: "One or more required fields are blank"});
+        if (!(firstName.match(/^[A-Za-z\-]+$/) )||(!(lastName.match(/^[A-Za-z\-]+$/))))
+            return res.status(400).json({msg: "Please ensure only letters are used for the first name and last name fields"});
+        if(password !== passwordCheck)
+            return res.status(400).json({msg: "Enter password twice to ensure password has been entered correctly"});
+
+        const emails = await User.find({},{email: 1});
+        for(let item of emails){
+            if (aesDecrypt(item.email) === email){
+                return res.status(400).json({msg: "An account with this email already exists."});
+            }
+        }
+        const phonenums = await User.find({},{phonenums: 1});
+        for(let item of phonenums){
+            if (aesDecrypt(item.email) === phoneNum){
+                return res.status(400).json({msg: "An account with this phone number already exists."});
+            }
+        }
+        /*const existingEmail = await User.findOne({email: email});
+        if(existingEmail)
+            return res.status(400).json({msg: "An account with this email already exists."});*/
+        const existingPID = await User.findOne({personalID: personalID});
+        if(existingPID)
+            return res.status(400).json({msg: "An account with this personal ID already exists."});
+        /*const existingPhoneNum = await User.findOne({phoneNum: phoneNum});
+        if(existingPhoneNum)
+            return res.status(400).json({msg: "An account with this phone number already exists."});*/
+        // if (!(phone.match(/\d{2}-\d{4}-\d{7}$/))) {
+        //     res.status(400).json({msg: "Telephone is not valid, please enter a valid phone number of the form XX--XXXX-XXXXXXX"});
+        // }
+        if (!(email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)))
+            return res.status(400).json({msg: "Email address is not valid, please enter a valid email, e.g. example@email.com"});
+        if (!(password.match(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,15}$/)))
+            return res.status(400).json({msg: "Password is not valid"});
+        if (!(personalID.match(/\d{11}$/)))
+            return res.status(400).json({msg: "Please enter exactly 11 digits for the personal ID number"});
+
+
 
         const salt = await bcrypt.genSalt();
         const passwordHash = await bcrypt.hash(password, salt);
-        console.log(passwordHash);
 
         const newUser = new User({
-            email,
+            email: aesEncrypt(email),
             password: passwordHash,
             personalID,
-            phoneNum,
-            firstName,
-            lastName
+            phoneNum: aesEncrypt(phoneNum),
+            firstName: aesEncrypt(firstName),
+            lastName: aesEncrypt(lastName),
         })
         const savedUser = await newUser.save();
         res.json(savedUser);
     } catch(err){
+        console.log(err);
         res.status(500).json({error: err.message} );
     }
 
 });
 router.post("/login", async (req, res) => {
-
-    function aesDecrypt(text) {
-        const crypto = require('crypto');
-        const key = req.body.key;
-        const iv = Buffer.from(req.body.iv, 'hex');
-        let encryptedText = Buffer.from(text, 'hex');
-        let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
-        let decrypted = decipher.update(encryptedText);
-        decrypted = Buffer.concat([decrypted, decipher.final()]);
-        return decrypted.toString();
-    }
 
     const {personalID, password} = req.body;
     //validate
@@ -88,20 +99,32 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({personalID: personalID});
     if(!user)
         return res.status(400).json({msg: "This user does not exist"});
-
+    console.log(user);
     const matchTrue = await bcrypt.compare(password, user.password);
     if(!matchTrue)
         return res.status(400).json({msg: "Incorrect password"})
     const token = jwt.sign({id: user._id}, process.env.JWT_PWD)
+
+    function aesDecrypt(data) {
+        const crypto = require('crypto');
+        const key = String(data.get('key'));
+        const iv = Buffer.from(String(data.get('iv')), 'hex');
+        let encryptedText = Buffer.from(String(data.get('encryptedData')), 'hex');
+        let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+        let decrypted = decipher.update(encryptedText);
+        decrypted = Buffer.concat([decrypted, decipher.final()]);
+        return decrypted.toString();
+    }
+
     res.json({
         token,
         user: {
             id: user._id,
             personalID: user.personalID,
-            email: user.email,
-            phoneNum: user.phoneNum,
-            firstName: user.firstName,
-            lastName: user.lastName
+            email: {data: aesDecrypt(user.email)},
+            phoneNum: {data: aesDecrypt(user.phoneNum)},
+            firstName: {data: aesDecrypt(user.firstName)},
+            lastName: {data: aesDecrypt(user.lastName)}
         },
     });
 });
