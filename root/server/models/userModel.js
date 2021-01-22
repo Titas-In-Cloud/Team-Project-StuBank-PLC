2c8c59141c5ce0784aa77bba6dc3d5a62a623b76
Titas Petelis (UG) <b9016816@newcastle.ac.uk>
2021-01-22 13:02:15 +0000

    Implemented server side from parent branch to work with front-end.

---
+++ root/server/models/userModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    email: {type: Map, of: String, required: true, unique: true},
+    password: {type: String, required: true, minlength: 8},
+    personalID: {type: String, required: true, unique: true},
+    phoneNum: {type: Map, of: String, required: true, unique: true},
+    firstName: {type: Map, of: String, required: true},
+    lastName: {type: Map, of: String, required: true},
+    accountBalance: {type: Number, required: true}
+});
+
+module.exports = User = mongoose.model("user", userSchema);
