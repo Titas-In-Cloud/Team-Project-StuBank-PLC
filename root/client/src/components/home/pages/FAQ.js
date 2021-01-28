fb8c090825b251d82943e4daad6ba8a68eb6e575
titas <t.petelis1@newcastle.ac.uk>
2021-01-28 09:27:16 +0000

    Added frontend style to FAQ page and changed names of the images.

--- root/client/src/components/home/pages/FAQ.js
+++ root/client/src/components/home/pages/FAQ.js
@@ -3,13 +3,36 @@ import { HomeNavigationBar } from "../../";
 
 export default function FAQ () {
     return (
-        <div className="default">
+        <div>
             <HomeNavigationBar />
-            <section>
-                <div className="centered-text-default">
-                    <h1>FAQ</h1>
+            <div className="faq-style">
+                <div className="container" style={{paddingTop: 50}}>
+                    <div className="message">
+                        <h1>Title</h1>
+                        <p>
+                            Description
+                        </p>
+                    </div>
+                    <div className="message">
+                        <h1>Title</h1>
+                        <p>
+                            Description
+                        </p>
+                    </div>
+                    <div className="message">
+                        <h1>Title</h1>
+                        <p>
+                            Description
+                        </p>
+                    </div>
+                    <div className="message">
+                        <h1>Title</h1>
+                        <p>
+                            Description
+                        </p>
+                    </div>
                 </div>
-            </section>
+            </div>
         </div>
     )
 }
